5cb38864c793339945f408df76c175b78d41ef99
Beom Sik <ryuha_____@naver.com>
2021-05-19 22:04:46 +0900

    first commit

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -4,11 +4,13 @@ function CityList(props) {
 
   return (
     <div>
-      <h1>CityList</h1>
+      <h1>Which city do you want to find?</h1>
+            <h1>CityList</h1>
 
       <ul>
         {cities.map((item, index) => {
-          return <li key={index}>{item}</li>;
+          return <li key={index}>
+          <a href= {'https://www.google.com/search?q='+item}> {item}</a></li>;
         })}
       </ul>
     </div>
